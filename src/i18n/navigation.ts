d63885c7fc434e20a0e48a0ecb6d73f0d2ac27db
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-12-21 12:39:29 +0200

    fix: locale is not optional

--- src/i18n/navigation.ts
+++ src/i18n/navigation.ts
@@ -4,4 +4,5 @@ import { i18n } from "./request";
 export const { Link, redirect, usePathname, useRouter } = createNavigation({
   locales: i18n.locales,
   defaultLocale: i18n.defaultLocale,
+  localePrefix: "as-needed",
 });
